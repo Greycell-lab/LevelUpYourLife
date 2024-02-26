4a9001503cfc849cb44370e95a9c59263251ec7f
Greycell <75478778+Greycell-lab@users.noreply.github.com>
2024-02-26 14:58:40 +0100

    Restcontroller only returns. New DataParser Class for Methods

--- src/main/java/de/domesoft/levelupapi/controller/DataController.java
+++ src/main/java/de/domesoft/levelupapi/controller/DataController.java
@@ -1,21 +1,14 @@
 package de.domesoft.levelupapi.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.domesoft.levelupapi.DataParser;
-import de.domesoft.levelupapi.PasswordHash;
 import de.domesoft.levelupapi.entity.Level;
 import de.domesoft.levelupapi.entity.LevelRepository;
-import de.domesoft.levelupapi.entity.User;
 import de.domesoft.levelupapi.entity.UserRepository;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.NoSuchAlgorithmException;
 
 @SuppressWarnings("unused")
 @RestController
@@ -43,23 +36,7 @@ public class DataController {
         return dataParser.postNewUser(data);
     }
     @PostMapping("/setexp")
-    public Level setExp(@RequestBody String data) throws Exception {
-        /*ObjectMapper mapper = new ObjectMapper();
-        JSONObject ob = new JSONObject(data);
-        Task task = ob.getEnum(Task.class, "task");
-        ob.remove("task");
-        System.out.println(ob.getJSONObject("user").toString());
-        ob.getJSONObject("user").put("passwordHash", PasswordHash.hash(ob.getJSONObject("user").getString("passwordHash")));
-        List<Level> levelList = levelRepository.getLevel(ob.getJSONObject("user").getString("userName"));
-        Level level = null;
-        for(Level l : levelList){
-            if(l.getName().equals(ob.getString("name"))){
-                level = l;
-            }
-        }
-        level.setExp(level.getExp() + task.getExp());
-        levelRepository.save(level);
-        return level;*/
+    public Level setExp(@RequestBody String data) {
         return null;
     }
 
